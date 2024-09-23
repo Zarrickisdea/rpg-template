1720ef6cf9a1945280e77af5b37f2cb427968788
Zarrickisdea <mohammedraz45@yahoo.com>
2024-09-24 01:47:02 +0530

    UI system init

---
+++ Assets/Scripts/UserInterface/UISystem/UISystem.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanel
+{
+
+}
+
+public class UISystem : MonoBehaviour
+{
+    private Stack<UIPanel> panelStack = new Stack<UIPanel>();
+}
